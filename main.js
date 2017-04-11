5d25db7ee7f9f5b24cf907a71b4bfd88e0da2e96
Sitar Harel <sitar@sitarharel.com>
2017-04-11 13:39:33 -0400

    finished up style tweens

--- main.js
+++ main.js
@@ -9,29 +9,36 @@ d3.queue()
     var states = pop.reduce((a, x) => {a[+x.STATE] ? a[+x.STATE] += +x[data] : a[+x.STATE] = +x[data]; return a}, [])
     var svg = d3.select("body").append("svg").attr("width", 600).attr("height", 400);
     
-    map = bubblemap().svg(svg)
-    .topology(us);
-
-    map();
+    map = bubblemap().svg(svg).topology(us);
+    
+    map().shape((d) => {d.no_clip = true; return d.state_shape}, 0)
 
     var tools = d3.select("body").append("div").attr("class", "toolbar");
 
     tools.append("button").text("layout-ify").on("click", () => {
-     map.shape((d) => { return d.origin_d}, 500);
-     map.size((d) => {d.bound_scale = true; return 1000}, 300);
-     map.location((d, i) => [(d.id%10) * 50 + 50, Math.floor(d.id/10) * 60 + 50]);
+      map.shape((d) => {return d.state_shape}, 500)
+      .location((d, i) => [(d.id%10) * 50 + 50, Math.floor(d.id/10) * 60 + 50])
+      .tween([
+        {style: "opacity", f: (d) => 0.5}, 
+        {attr: "area", f: (d) => {d.no_clip = true; d.bound_scale = true; return 1000}},
+        {style: "fill", interpolator: d3.interpolateRgb, f: (d) => "green"}
+      ], 500);
     });
 
     tools.append("button").text("circle-ify").on("click", () => {
-      map.shape((d) => d.d, 200);
-      map.size((d) => {d.bound_scale = false; return d.origin_area}, 200);
-      map.location((d, i) => [d.geo_origin_x, d.geo_origin_y])
+      map.shape((d) => d.circle_path, 200)
+      .location((d) => d.geo_origin)
+      .tween([
+        {style: "opacity", f: (d) => 1}, 
+        {attr: "area", f: (d) => { d.no_clip = false; d.bound_scale = false; return d.origin_area}},
+        {style: "fill", interpolator: d3.interpolateRgb, f: (d) => "royalblue"}
+      ], 300)
     });
 
     tools.append("button").text("state-ify").on("click", () => {
-      map.shape((d) => d.origin_d, 1000);
-      // map.size((d) => {d.bound_scale = false; return d.origin_area}, 200);
-      map.location((d, i) => [d.geo_origin_x, d.geo_origin_y])
+      map.shape((d) => d.state_shape, 1000)
+      .location((d) => d.geo_origin)
+      .tween({style: "fill", interpolator: d3.interpolateRgb, f: (d) => "purple"}, 1000)
     });
 
 });
--- map.js
+++ map.js
@@ -3,83 +3,52 @@ var bubblemap = function(){
 
   function bm(){
     simulation = d3.forceSimulation()
-        .force("x_pos", d3.forceX(d => d.root_x))
-        .force("y_pos", d3.forceY(d => d.root_y))
-        .force("collision", d3.forceCollide(d => d.r))
-        .force("charge", d3.forceManyBody())
+    .force("collision", d3.forceCollide(d => d.no_clip ? 0 : Math.sqrt(d.area/Math.PI) ))
+    .force("x_pos", d3.forceX(d => d.root[0]))
+    .force("y_pos", d3.forceY(d => d.root[1]))
 
     nodedata = gennodes(states);
 
-    function reboot(c) {
-      node = svg
-        .selectAll(".node")
-        .data(nodedata);
-      
-      node.exit().remove();
+    node = svg
+      .selectAll(".node")
+      .data(nodedata);
 
-      node = svg
-        .selectAll(".node")
-        .data(nodedata);
+    node.exit().remove();
 
-      node.exit().remove();
+    node_vis = node
+    .enter().append("path")
+    .merge(node)
+    .attr("class", "node")
+    .attr("d", (s) => s.circle_path)
+    .call(d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended))
+    .on("click", (d) => console.log(d)); 
 
-      node_vis = node
-        .enter().append("path")
-        .merge(node)
-        .attr("class", "node")
-        .attr("d", (s) => s.d)
-        // .style("fill", "none")
-        .style("fill", function(d,i) { return d.fill})
-        // .style("stroke", "black")
-        .style("fill-rule", "nonzero")
-        .call(d3.drag()
-          .on("start", dragstarted)
-          .on("drag", dragged)
-          .on("end", dragended))
-        .on("click", (d) => {
-          if(d.id > 100) return;
-          var curr_state = d.id; 
-          console.log(d);
-          // reboot(gennodes(counties(curr_state), 2, {id: d.id}))
-        }); 
+    simulation.nodes(nodedata).on("tick", on_tick);
+    simulation.force("x_pos").strength((d) => 0.5);
+    simulation.force("y_pos").strength((d) => 0.5);
+    simulation.alpha(1).restart();
 
-      simulation
-          .nodes(nodedata)
-          .on("tick", ticked);
+    // this gets called every tick. used to do force stuff but also for reactive updating
+    function on_tick() {
+      simulation.force("collision").radius((d) => {return d.no_clip ? 0 : Math.sqrt(d.area/Math.PI)})
+      simulation.force("x_pos").x((d) => d.root[0])
+      simulation.force("y_pos").y((d) => d.root[1])
 
-      simulation.force("x_pos").strength((d) => d.parent_id ? 1.5 : 0.5);
-      simulation.force("y_pos").strength((d) => d.parent_id ? 1.5 : 0.5);
-      simulation.force("charge").strength((d) => d.parent_id ? -7 : 0);
+      // set the node's location. (node paths should be centered around d.geo_origin_area)
+      node_vis.attr("transform", function(d) { 
+        var scale = Math.sqrt(d.area/d.geo_origin_area);
+        if(d.bound_scale) scale = Math.sqrt(d.area/d.bound_origin_area);
+        return "translate(" + d.x + ", " + d.y + ") scale("+ scale +") translate(" + (-d.geo_origin[0]) + ", " + (-d.geo_origin[1]) + ")";
+      });
 
-      function ticked() {
-        simulation.force("collision").radius((d) => {return d.no_clip ? 0 : Math.sqrt(d.area/Math.PI)})
-        simulation.force("x_pos").x((d) => d.root_x)
-        simulation.force("y_pos").y((d) => d.root_y)
-
-        node_vis
-        .attr("transform", function(d) { 
-          var scale = Math.sqrt(d.area/d.geo_origin_area);
-          if(d.bound_scale) scale = Math.sqrt(d.area/d.bound_origin_area);
-          return "translate(" + d.x + ", " + d.y + ") scale("+ scale +") translate(" + (-d.geo_origin_x) + ", " + (-d.geo_origin_y) + ")";
-        })
-
-        // .attr("d", (d) => d.d);
-        // node_vis
-        //     // .style("opacity", function(d) { return d.id == curr_state ? 0 : 1; })
-        //     .attr("r", (d) => d.r)
-        //     .style("fill", (d) => d.fill)
-        //     .attr("cx", (d) => d.x)
-        //     .attr("cy", (d) => d.y);
-// 
-            // debugs.attr("r", (d) => 5)
-            // .attr("cx", (d) => d.x)
-            // .attr("cy", (d) => d.y);
-           
-      }
-
-      simulation.alpha(1).restart();
+      // Set the node style for every style attribute
+      Object.keys(node_vis.datum().style).forEach((key) => {
+        node_vis.style(key, d => d.style[key])
+      });
     }
-    reboot([]);
 
     return bm;
   }
@@ -98,32 +67,6 @@ var bubblemap = function(){
     pathGenerator = d3.geoPath().projection(projection);
     projection.fitExtent([[0,0], [width, height]], states);
 
-    centerStatePath = function(state_d){
-      var trans = projection.translate();
-      var center = pathGenerator.centroid(state_d).map(d => -d);
-      center[0] += width/2;
-      center[1] += height/2;
-      var res = d3.geoPath().projection(projection.translate(center))(state_d);
-      // console.log(geoPath)
-
-    // svg.append("rect")
-    // .attr("x", bounds[0][0])
-    // .attr("y", bounds[0][1])
-    // .attr("width", (bounds[1][0]-bounds[0][0]))
-    // .attr("height", (bounds[1][1]-bounds[0][1]))
-    // .style("fill", "none")
-    // .style("stroke", "black");
-    // svg.append("circle")
-    // .attr("cx", center[0])
-    // .attr("cy", center[1])
-    // .attr("r", 5)
-    // .style("fill", "yellow")
-    // .style("stroke", "black");
-
-      projection.translate(trans);
-      return res;
-    }
-
     return bm;
   }
 
@@ -135,99 +78,77 @@ var bubblemap = function(){
     return bm;
   }
 
-  /* inputted data should look like this: 
-   * {
-   *   "states": [array mapping FIPS to data], 
-   *   "counties" [array mapping county FIPS to data]
-   * }
-   */
-  bm.data = function(d){
-    if(!d) return data;
-    data = d;
-    return bm;  
-  }
-
   /* f is a function that maps data to location 
    * ex: location((d, i) => { return [i * 10, 50]; }) 
    */
   bm.location = function(f, duration){
-    nodedata.forEach((d, i) => {
-      var upd = f(d, i);
-      d.root_x = upd[0];
-      d.root_y = upd[1];
-    });
+    nodedata.forEach((d, i) => d.root = f(d, i));
     simulation.alphaTarget(0.5).restart();
-
-    return bm;
-  }
-
-  bm.size = function(f, duration){
-    bm.tween(f, "area", duration, d3.interpolateNumber);
     return bm;
   }
 
   bm.shape = function(f, duration){
-    node_vis
-    .transition().duration(duration)
-    .attr("d", f);
-
+    node_vis.transition().duration(duration).attr("d", f);
     return bm;
   }
 
-  bm.tween = function(f, attr, duration, interpolator){
+  bm.tween = function(attrs, duration){
+    if(!Array.isArray(attrs)) attrs = [attrs];
+    var tween_func = function(d){
+      return function(t){
+        return attrs.forEach((a) => {
+          dataTween(d, a.f, a.style ? a.style : a.attr, a.interpolator, a.style)(t);
+        });
+      }
+    }
     node.enter() // change to node_vis
     .transition().duration(duration)
-    .tween("datum", dataTween(f, attr, interpolator));
-    // console.log("tweening " + attr);
+    .tween("datum", tween_func);
     simulation.alphaTarget(0.5).restart();
     return bm;
   }
 
-  /* f is a function that generates the tween's "to" value */
-  function dataTween(f, attr, interpolator){
+  /* this generates a single tweening function for attribute or style attr
+   * f is a function that generates the tween's "to" value 
+   */
+  function dataTween(d, f, attr, interpolator, is_style){
     interpolator = interpolator || d3.interpolateNumber;
-    return function(d){
-      var i = interpolator(d[attr], f(d));
+    is_style = is_style || false;
+      var i = interpolator(is_style ? d.style[attr] : d[attr], f(d));
       return function(t){
-        d[attr] = i(t);
-        // console.log(d[attr])
-        return d[attr];
+        if(is_style) d.style[attr] = i(t);
+        else d[attr] = i(t);
       }
-    }
   }
 
-  function gennodes(topo, inv_size, parent){
-    inv_size = inv_size || 2;
-
+  /* generates default node data for states defined by topo */
+  function gennodes(topo){
     return topo.features.map((d) => { 
-    // var npath = centerStatePath(d);
-    var center = pathGenerator.centroid(d);
-    // console.log(npath);
     var bounds = pathGenerator.bounds(d);
-    // var center = [(bounds[1][0]+bounds[0][0])/2, (bounds[1][1]+bounds[0][1])/2]
     var bound_size = (bounds[1][0]-bounds[0][0]) * (bounds[1][1]-bounds[0][1])
+    var center = pathGenerator.centroid(d);
 
     return {
-      d: mergablepath(d, Math.sqrt(pathGenerator.area(d)/Math.PI), center[0], center[1]),
-      r: Math.sqrt(pathGenerator.area(d)/Math.PI), 
+      circle_path: mergablepath(d, Math.sqrt(pathGenerator.area(d)/Math.PI), center[0], center[1]), // a mergable circle of the path
       area: pathGenerator.area(d), 
       origin_area: pathGenerator.area(d),
       geo_origin_area: pathGenerator.area(d),
       bound_origin_area: bound_size,
-      bound_scale: false,
+      bound_scale: false, // whether to scale state based on bound size or path area
       x: center[0], 
       y: center[1], 
-      root_x: center[0], 
-      root_y: center[1],
-      geo_origin_x: center[0], 
-      geo_origin_y: center[1],
-      id: d.id,
-      is_circle: true,
-      no_clip: false,
-      fill: "royalblue",
-      parent_id: parent ? parent.id : null,
-      origin_d: pathGenerator(d),
-      origin_shape: d
+      root: center, // roots are the locations that objects are attracted to
+      geo_origin: center, // geo_origin is the offset value to the center of the shape in the path
+                          // ex: a square from -5,-5 to 15,15 would have geo_origin = [5, 5]
+      id: d.id, // id - usually the FIPS code for each state
+      no_clip: false, // whether or not it should collide with others
+      no_drag: false, // whether or not user should be able to interact with nodes
+      state_shape: pathGenerator(d), // shape of the state
+      origin_shape: d, 
+      style: {
+        fill: "royalblue",
+        opacity: 1
+      }
     }}).filter(d => d.x && d.id != 11);
   }
 
@@ -250,7 +171,6 @@ var bubblemap = function(){
     // var len = coords.length;
     var len = ((pathGenerator(d) || "").match(/L|M/g) || []).length
     var angleoffset = 2 * Math.PI / len;
-    // var angle = -Math.PI * 3 / 4;
     var angle = 0;
     var res = "M ";
     for (var i = 0; i < len - 1; i++){
@@ -260,22 +180,24 @@ var bubblemap = function(){
     }
     res = res +  " " + (x + r * Math.cos(angle)) + 
       " " + (y + r * Math.sin(angle)) + " Z"    // centroid = polygon.centroid(-1 / (6 * area)),
-    // console.log(res);
     return res;
   }
 
   function dragstarted(d) {
+    if(d.no_drag) return;
     if (!d3.event.active) simulation.alphaTarget(0.3).restart();
     d.fx = d.x;
     d.fy = d.y;
   }
 
   function dragged(d) {
+    if(d.no_drag) return;
     d.fx = d3.event.x;
     d.fy = d3.event.y;
   }
 
   function dragended(d) {
+    if(d.no_drag) return;
     if (!d3.event.active) simulation.alphaTarget(0);
     d.fx = null;
     d.fy = null;
